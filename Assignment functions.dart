8bf5fb67aad9fd1c7bf04914f17d49b9af25efc3
Kamau Faith <160272422+faith00206@users.noreply.github.com>
2024-03-11 18:09:51 +0300

    Add files via upload

---
+++ Assignment functions.dart
@@ -0,0 +1,43 @@
+// task 1
+import 'dart:js_util';
+
+void add =(int numba, numbb){
+int; add = numba + numbb;
+print('the sum is &sum');
+};
+// task 2 subtract 2
+void suntractTwo =( int numbK,numbZ){
+  int; subtract(numbK, numbZ);
+  print('the subtraction is &subtract');
+};
+// task 3 multiply 2
+void multiplyTwo=( int numbE, numbD){
+int; multiplyTwo= numbE * numbD;
+print('the multiplication is & multiply');
+};
+//task 4 divide two
+Object divideTwo(double a, double b) {
+  if (b != 0) {
+    return a / b;
+  } else {
+    print("Cannot divide by zero!");
+    return false;
+  }
+}
+// task 5 string length
+int stringLength(String inputString) {
+  return inputString.length;
+}
+// task 6 getting the first element
+dynamic getFirstElement(List<dynamic> myList) {
+  if (myList.isNotEmpty) {
+    return myList[0];
+  } else {
+    print("The list is empty!");
+    return null;
+  }
+}
+
+
+
+
